82e2d0e463c669621fdf3594de22a2aaacb6334f
l <l@ls-MacBook-Air.local>
2025-08-18 00:34:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Accordion from './LiftingState/exam';
+import SyncedInputs from './LiftingState/Synced_inputs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Accordion/>
+      <hr></hr>
+      <h1>Challenge 1 of 2: Synced inputs</h1>
     </div>
   );
 }
---
+++ src/LiftingState/Synced_inputs.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export default function SyncedInputs() {
+  return (
+    <>
+      <Input label="First input" />
+      <Input label="Second input" />
+    </>
+  );
+}
+// Pass hardcoded data from  the common parent - 2
+function Input({ label, onChange, value  }) {
+    // Remove State from the child Components   -> 1
+//   const [text, setText] = useState('');
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <label>
+      {label}
+      {' '}
+      <input
+        value={text}
+        onChange={handleChange}
+      />
+    </label>
+  );
+}
