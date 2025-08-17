ee65b34bd6fc8d198472a196f4c67875dac8b768
Islam Atwa <islamatwa429@gmail.com.com>
2025-08-18 01:15:37 +0300

    step 1: Removed State From Input Components

--- src/LiftingState/Synced_inputs.js
+++ src/LiftingState/Synced_inputs.js
@@ -10,7 +10,7 @@ export default function SyncedInputs() {
 }
 
 function Input({ label }) {
-  const [text, setText] = useState('');
+  
 
   function handleChange(e) {
     setText(e.target.value);
@@ -27,3 +27,11 @@ function Input({ label }) {
     </label>
   );
 }
+
+
+// structure 
+// To  solve this challange  i will need 3 step:
+//  - step 1: Remove State From Child ->(Input) Components 
+//  - step 2: Pass hardedcoded date from the common parent to the child components
+//  - step 3 : Add a state to the common parent and pass it down to the child components
+  
