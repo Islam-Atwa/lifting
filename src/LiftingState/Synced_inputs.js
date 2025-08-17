cf835acd9902dfd4ac5149e32ee28cc963ad2fc2
Islam Atwa <islamatwa429@gmail.com.com>
2025-08-18 01:08:29 +0300

    Code Before Solution

--- src/LiftingState/Synced_inputs.js
+++ src/LiftingState/Synced_inputs.js
@@ -8,10 +8,9 @@ export default function SyncedInputs() {
     </>
   );
 }
-// Pass hardcoded data from  the common parent - 2
-function Input({ label, onChange, value  }) {
-    // Remove State from the child Components   -> 1
-//   const [text, setText] = useState('');
+
+function Input({ label }) {
+  const [text, setText] = useState('');
 
   function handleChange(e) {
     setText(e.target.value);
